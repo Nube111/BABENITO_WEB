cd289db04fcb44cb142cf3b81108e8c780ab3de9
SGDIEGO <diegoalonsosg@gmail.com>
2024-11-29 11:52:12 -0500

    modifing styles

--- FRONT/src/Paginas/crearpedido/indexcrepe.tsx
+++ FRONT/src/Paginas/crearpedido/indexcrepe.tsx
@@ -3,111 +3,87 @@ import "./indexcrepe.css";
 
 function CrearPedido(): JSX.Element {
   return (
-    <div className="container">
-      <header className="header">
-        <h1 className="logo">BABENITO</h1>
+    <div className="crear-pedido-container">
+      <header className="crear-pedido-header">
+        <h1 className="crear-pedido-logo">BABENITO</h1>
         <input
           type="text"
           placeholder="Busque pedidos por fecha, estado o código..."
-          className="search-bar"
+          className="crear-pedido-search-bar"
         />
-        <div className="menu">
+        <div className="crear-pedido-menu">
           <span>Modelos</span>
           <span>Clientes</span>
           <img
             src="./menu-icon.png" // Cambia por el ícono de menú real
             alt="Menú"
-            className="menu-icon"
+            className="crear-pedido-menu-icon"
           />
         </div>
       </header>
-      <main className="main-content">
-        <div className="breadcrumb">
+      <main className="crear-pedido-main-content">
+        <div className="crear-pedido-breadcrumb">
           <a href="#">Pedidos</a> &gt; <span>Crear pedido</span>
         </div>
-        <section className="model-section">
-          <div className="model-left">
-            <label htmlFor="model-code" className="section-label">
+        <section className="crear-pedido-model-section">
+          <div className="crear-pedido-model-left">
+            <label htmlFor="model-code" className="crear-pedido-section-label">
               Elección de modelo
             </label>
             <input
               id="model-code"
               type="text"
               placeholder="Código de modelo..."
-              className="model-input"
+              className="crear-pedido-model-input"
             />
-            <div className="model-preview">
-              <img src="./shoes.jpg" alt="Modelo" className="model-image" />
-              <p className="model-title">Modelo "código de modelo"</p>
-              <p className="model-description">Datos esenciales del modelo...</p>
+            <div className="crear-pedido-model-preview">
+              <img src="./shoes.jpg" alt="Modelo" className="crear-pedido-model-image" />
+              <p className="crear-pedido-model-title">Modelo "código de modelo"</p>
+              <p className="crear-pedido-model-description">Datos esenciales del modelo...</p>
             </div>
           </div>
-          <div className="model-right">
-            <p className="model-info">
+          <div className="crear-pedido-model-right">
+            <p className="crear-pedido-model-info">
               En caso de no recordar el código del modelo en la parte derecha
               tenemos un botón de lista de modelos donde podremos visualizar
               todos los modelos y sus códigos, puede seleccionar el modelo que
               se busca y el código se autocompletará. En caso de seleccionar el
               modelo equivocado puede volver a seleccionar el botón lista de
-              modelos y volver a elegir.
             </p>
-            <button className="button-secondary">Lista de modelos</button>
           </div>
         </section>
-        <section className="order-section">
-          <h3 className="section-label">Datos específicos del pedido</h3>
-          <div className="order-details">
-            <input
-              type="text"
-              placeholder="Escribir serie..."
-              className="order-input"
-            />
-            <button className="button-primary">Buscar serie</button>
-            <div className="sizes">
-              <label className="size-label">Cantidad por tallas:</label>
-              <input type="text" placeholder="14" className="size-input" />
-              <input type="text" placeholder="15" className="size-input" />
-              <input type="text" placeholder="16" className="size-input" />
-            </div>
+        <section className="crear-pedido-attributes-section">
+          <div className="crear-pedido-attribute">
+            <label htmlFor="forro" className="crear-pedido-section-label">Forro</label>
+            <input id="forro" type="text" placeholder="Forro..." className="crear-pedido-model-input" />
+            <input id="forro-color" type="text" placeholder="Color del forro..." className="crear-pedido-model-input" />
+            <input id="forro-material" type="text" placeholder="Material del forro..." className="crear-pedido-model-input" />
+          </div>
+          <div className="crear-pedido-attribute">
+            <label htmlFor="hilo" className="crear-pedido-section-label">Hilo</label>
+            <input id="hilo" type="text" placeholder="Hilo..." className="crear-pedido-model-input" />
+            <input id="hilo-color" type="text" placeholder="Color del hilo..." className="crear-pedido-model-input" />
+            <input id="hilo-material" type="text" placeholder="Material del hilo..." className="crear-pedido-model-input" />
+          </div>
+          <div className="crear-pedido-attribute">
+            <label htmlFor="adorno" className="crear-pedido-section-label">Adorno</label>
+            <input id="adorno" type="text" placeholder="Adorno..." className="crear-pedido-model-input" />
+            <input id="adorno-color" type="text" placeholder="Color del adorno..." className="crear-pedido-model-input" />
+            <input id="adorno-material" type="text" placeholder="Material del adorno..." className="crear-pedido-model-input" />
+          </div>
+          <div className="crear-pedido-attribute">
+            <label htmlFor="picadores" className="crear-pedido-section-label">Picadores</label>
+            <input id="picadores" type="text" placeholder="Picadores..." className="crear-pedido-model-input" />
+            <input id="picadores-color" type="text" placeholder="Color de los picadores..." className="crear-pedido-model-input" />
+            <input id="picadores-material" type="text" placeholder="Material de los picadores..." className="crear-pedido-model-input" />
+          </div>
+          <div className="crear-pedido-attribute">
+            <label htmlFor="apliques" className="crear-pedido-section-label">Apliques</label>
+            <input id="apliques" type="text" placeholder="Apliques..." className="crear-pedido-model-input" />
+            <input id="apliques-color" type="text" placeholder="Color de los apliques..." className="crear-pedido-model-input" />
+            <input id="apliques-material" type="text" placeholder="Material de los apliques..." className="crear-pedido-model-input" />
           </div>
         </section>
-        <section className="client-section">
-          <h3 className="section-label">Cliente</h3>
-          <div className="client-existing">
-            <label htmlFor="existing-client" className="client-label">
-              Cliente existente:
-            </label>
-            <input
-              id="existing-client"
-              type="text"
-              placeholder="Nombre del cliente..."
-              className="client-input"
-            />
-            <button className="button-primary">Buscar cliente existente</button>
-          </div>
-          <div className="client-new">
-            <h4>Cliente nuevo:</h4>
-            <div className="client-grid">
-              <input type="text" placeholder="Primer nombre" />
-              <input type="text" placeholder="Primer apellido" />
-              <input type="text" placeholder="RUC" />
-              <input type="text" placeholder="DNI" />
-              <input type="text" placeholder="Segundo nombre" />
-              <input type="text" placeholder="Segundo apellido" />
-              <input type="text" placeholder="Razón social" />
-              <input type="text" placeholder="Precio" />
-            </div>
-          </div>
-        </section>
-        <div className="order-actions">
-          <p className="instructions">
-            Si completaste todos los datos requeridos, puedes darle a crear
-            pedido para finalizar el proceso.
-          </p>
-          <button className="button-primary create-order-button">
-            Crear pedido
-          </button>
-        </div>
       </main>
     </div>
   );
